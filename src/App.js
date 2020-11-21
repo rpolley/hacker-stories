7c6e9b095274c2ea57f9149fc09cf0f09d37ab53
Ryan Polley <ryanpolley@localhost.localdomain>
2020-11-21 14:06:40 -0600

    Another React Component

--- src/App.js
+++ src/App.js
@@ -23,19 +23,22 @@ function App() {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
       <hr />
-      {list.map(function(item) {
-        return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
+      <List/>
     </div>
   );
+}
+function List() {
+  return list.map(function(item) {
+    return (
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </div>
+    );
+  })
 }
 export default App;
