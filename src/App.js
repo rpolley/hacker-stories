847ba457e9c5d9f4f888112dd77fc0654113f759
Ryan Polley <rypolley@gmail.com>
2020-11-24 11:56:00 -0600

    react props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
   const handleChange =  event => {
     console.log(event);
   }
@@ -26,13 +26,12 @@ const App = () => {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange}/>
       <hr />
-      <List/>
-      <List/>
+      <List list={stories}/>
     </div>
   );
 }
-function List() {
-  return list.map(function(item) {
+const List = props => {
+  return props.list.map(item => {
     return (
       <div key={item.objectID}>
         <span>
