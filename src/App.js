31daed321259767def7c872ed3838ce1dad83d46
Ryan Polley <rypolley@gmail.com>
2020-12-03 15:06:13 -0600

    React controlled components

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchTerm, setSearchTerm] = React.useState('React');
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
@@ -34,7 +34,7 @@ const App = () => {
     <div>
       <h1>Hello React</h1>
       
-      <Search onSearch={handleSearch}/>
+      <Search search={searchTerm} onSearch={handleSearch}/>
 
       <hr />
       <List list={searchedStories}/>
@@ -45,7 +45,12 @@ const Search = props => {
   return (
     <div> 
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearch}/>
+      <input
+        id="search" 
+        type="text"
+        value={props.search}
+        onChange={props.onSearch}
+      />
     </div>
   );
 }
