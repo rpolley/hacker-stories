ec948ba0cd454b38365ebbc0f7f9148d99518a12
Ryan Polley <rypolley@gmail.com>
2020-12-04 19:36:22 -0600

    Data Fetching with React

--- src/App.js
+++ src/App.js
@@ -64,6 +64,8 @@ const storiesReducer = (state, action) => {
   }
 }
 
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
@@ -79,18 +81,21 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories().then(result => {
+    fetch(`${API_ENDPOINT}react`)
+    .then(response => response.json())
+    .then(result => {
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: result.data.stories,
+        payload: result.hits,
       });
     })
-    .catch(() =>
+    .catch((err) => {
+      console.log(err);
       dispatchStories({
         type: 'STORIES_FETCH_FAILURE',
         payload: [],
       })
-    );
+    });
   }, []);
 
   const handleRemoveStory = item => {
